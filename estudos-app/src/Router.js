66a8e89cff3c47818135a7606dddd83c59934652
andre_nascimento <contato.nascimentoandre@gmail.com>
2020-04-30 21:56:56 -0300

    View login criada

--- estudos-app/src/Router.js
+++ estudos-app/src/Router.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Home from './view/home';
+import Login from './view/Login';
 
 import { Switch, Route, HashRouter } from 'react-router-dom'
+
 
 export default () => {
     return (
         <Switch>
             <Route exact path='/' component={Home}/>
+            <Route exact path='/Login' componet={Login}/>
         </Switch>
     )
 }
---
+++ estudos-app/src/view/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function loginScreen() {
+    return (
+        <form>
+            <input type="text">Name</input>
+            <inpur type="password">Password</inpur>
+            <input type="submit">Send</input>
+        </form>
+    )
+}
+
+export default loginScreen;
